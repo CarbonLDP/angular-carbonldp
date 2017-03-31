f421883f5549417d9e8bfd5e0db984390d155c77
Alvaro Contreras <alvaro.contreras@base22.com>
2017-03-30 19:44:24 -0600

    Compile dist files

--- dist/guards/not-authenticated.guard.js
+++ dist/guards/not-authenticated.guard.js
@@ -4,15 +4,6 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
 var abstract_authentication_guard_1 = require("./abstract-authentication.guard");
@@ -40,8 +31,11 @@ var NotAuthenticatedGuard = (function (_super) {
     };
     return NotAuthenticatedGuard;
 }(abstract_authentication_guard_1.AbstractAuthenticationGuard));
-NotAuthenticatedGuard = __decorate([
-    core_1.Injectable(),
-    __metadata("design:paramtypes", [router_1.Router])
-], NotAuthenticatedGuard);
+NotAuthenticatedGuard.decorators = [
+    { type: core_1.Injectable },
+];
+/** @nocollapse */
+NotAuthenticatedGuard.ctorParameters = function () { return [
+    { type: router_1.Router, },
+]; };
 exports.NotAuthenticatedGuard = NotAuthenticatedGuard;
