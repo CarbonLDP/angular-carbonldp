34a49b7ed1a49f100ecb547f6faaca582ac52935
Alvaro Contreras <alvaro.contreras@base22.com>
2017-05-09 16:25:22 -0500

    Change active-content resolver to carbon-provider resolver

--- src/resolvers/carbon-provider.resolver.ts
+++ src/resolvers/carbon-provider.resolver.ts
@@ -3,22 +3,22 @@ import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 import { Router, Resolve, ActivatedRouteSnapshot } from "@angular/router";
 
-import Context from "carbonldp/Context";
+import { Class as Carbon } from "carbonldp/Carbon";
 
-import { activeContext } from "./../boot";
+import { carbonProvider } from "./../boot";
 
 @Injectable()
-export class ActiveContextResolver implements Resolve<Context> {
+export class CarbonProviderResolver implements Resolve<Carbon> {
 	constructor( protected router:Router ) {}
 
-	resolve( route:ActivatedRouteSnapshot ):Observable<Context> | Promise<Context> | Context {
-		return activeContext.promise.then( () => {
-			return activeContext();
+	resolve( route:ActivatedRouteSnapshot ):Observable<Carbon> | Promise<Carbon> | Carbon {
+		return carbonProvider.promise.then( () => {
+			return carbonProvider();
 		} ).catch( ( error ) => {
 			if( typeof route.data === "object" && route.data !== null && typeof route.data[ "onError" ] !== "undefined" ) {
 				this.router.navigate( route.data[ "onError" ] );
 			} else {
-				console.error( "ActiveContextResolver was configured in a route without an 'data.onError' property" );
+				console.error( "CarbonProviderResolver was configured in a route without an 'data.onError' property" );
 			}
 
 			return false;
